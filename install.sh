a1a92617aa80226a6901e345ed09ecfdd1d96ca6
Eslam Mohamed <84045971+Crypt00o@users.noreply.github.com>
2022-06-10 14:22:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ done
 printf "${GREEN}"
 printf " %%100 "  
 Install() {
-apt install ettercap-common netdicover ; cd build ; bash build.sh
+apt install ettercap-common netdiscover ; cd build ; bash build.sh
 }
 Install
 printf "${YELLOW}"
